8b6528d404acd066c08f95bcabfafd6ff9161bca
Suniksha <sunikshabenpatel@gmail.com>
2025-01-15 16:15:45 +0530

    Part-4 java ouput and comment

---
+++ Part-4 Java Output/First_pro.java
@@ -0,0 +1,11 @@
+public class First_pro {
+    public static void main(String[] args) {
+        // This will print the number 
+       System.out.println(3+3);
+       System.out.println(358 - 48);
+       System.out.println(5000 / 50); // this is performing division oprtaions
+       /* this code will perform mathematical caluculations
+        * it will print the result of the mathematical operation
+       */
+    }
+}
