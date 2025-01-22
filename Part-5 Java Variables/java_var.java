fc9ad36981cd1559563754755d76288911435e45
Suniksha <sunikshabenpatel@gmail.com>
2025-01-22 22:53:22 +0530

    Part-5 Java Varibles

---
+++ Part-5 Java Variables/java_var.java
@@ -0,0 +1,16 @@
+//real life example
+public class java_var {
+    public static void main(String[] args) {
+      //calulation of Are of a rectangle
+      int length = 4;
+      int width = 6;
+      int area;
+
+      area = length * width ;
+
+      //print varibales
+      System.out.println("length = " + length);
+      System.out.println("width = " + width);
+      System.out.println("area = " + area);
+    }
+}
